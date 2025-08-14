ce461db7e6c3cc9c9425033bff4472338efa8bae
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-08-14 05:56:05 +0000

    Only 4 runs in parallel

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-export XLA_PYTHON_CLIENT_MEM_FRACTION=0.05
+export XLA_PYTHON_CLIENT_MEM_FRACTION=0.2
 unset JAX_PLATFORMS
 
 for i in {0..4}
@@ -10,7 +10,7 @@ do
   python run_ac_offline.py --seed $i --env_name Ant --dataset medium --discrete_control 0 --state_dim 111 --action_dim 8 --tau 0.5 --learning_rate 0.0003 --hidden_units 256 --batch_size 256 --timeout 1000 --max_steps 1000000 --log_interval 10000 &
   sleep 1
   python run_ac_offline.py --seed $i --env_name Ant --dataset medrep --discrete_control 0 --state_dim 111 --action_dim 8 --tau 0.5 --learning_rate 0.0003 --hidden_units 256 --batch_size 256 --timeout 1000 --max_steps 1000000 --log_interval 10000 &
-  sleep 1
+  wait
   python run_ac_offline.py --seed $i --env_name HalfCheetah --dataset expert --discrete_control 0 --state_dim 17 --action_dim 6 --tau 0.01 --learning_rate 0.0003 --hidden_units 256 --batch_size 256 --timeout 1000 --max_steps 1000000 --log_interval 10000 &
   sleep 1
   python run_ac_offline.py --seed $i --env_name HalfCheetah --dataset medexp --discrete_control 0 --state_dim 17 --action_dim 6 --tau 0.1 --learning_rate 0.0003 --hidden_units 256 --batch_size 256 --timeout 1000 --max_steps 1000000 --log_interval 10000 &
@@ -18,7 +18,7 @@ do
   python run_ac_offline.py --seed $i --env_name HalfCheetah --dataset medium --discrete_control 0 --state_dim 17 --action_dim 6 --tau 0.33 --learning_rate 0.0003 --hidden_units 256 --batch_size 256 --timeout 1000 --max_steps 1000000 --log_interval 10000 &
   sleep 1
   python run_ac_offline.py --seed $i --env_name HalfCheetah --dataset medrep --discrete_control 0 --state_dim 17 --action_dim 6 --tau 0.5 --learning_rate 0.0003 --hidden_units 256 --batch_size 256 --timeout 1000 --max_steps 1000000 --log_interval 10000 &
-  sleep 1
+  wait
   python run_ac_offline.py --seed $i --env_name Hopper --dataset expert --discrete_control 0 --state_dim 11 --action_dim 3 --tau 0.01 --learning_rate 0.0003 --hidden_units 256 --batch_size 256 --timeout 1000 --max_steps 1000000 --log_interval 10000 &
   sleep 1
   python run_ac_offline.py --seed $i --env_name Hopper --dataset medexp --discrete_control 0 --state_dim 11 --action_dim 3 --tau 0.01 --learning_rate 0.0003 --hidden_units 256 --batch_size 256 --timeout 1000 --max_steps 1000000 --log_interval 10000 &
@@ -26,7 +26,7 @@ do
   python run_ac_offline.py --seed $i --env_name Hopper --dataset medium --discrete_control 0 --state_dim 11 --action_dim 3 --tau 0.1 --learning_rate 0.0003 --hidden_units 256 --batch_size 256 --timeout 1000 --max_steps 1000000 --log_interval 10000 &
   sleep 1
   python run_ac_offline.py --seed $i --env_name Hopper --dataset medrep --discrete_control 0 --state_dim 11 --action_dim 3 --tau 0.5 --learning_rate 0.0003 --hidden_units 256 --batch_size 256 --timeout 1000 --max_steps 1000000 --log_interval 10000 &
-  sleep 1
+  wait
   python run_ac_offline.py --seed $i --env_name Walker2d --dataset expert --discrete_control 0 --state_dim 17 --action_dim 6 --tau 0.01 --learning_rate 0.0003 --hidden_units 256 --batch_size 256 --timeout 1000 --max_steps 1000000 --log_interval 10000 &
   sleep 1
   python run_ac_offline.py --seed $i --env_name Walker2d --dataset medexp --discrete_control 0 --state_dim 17 --action_dim 6 --tau 0.1 --learning_rate 0.0003 --hidden_units 256 --batch_size 256 --timeout 1000 --max_steps 1000000 --log_interval 10000 &
@@ -34,6 +34,6 @@ do
   python run_ac_offline.py --seed $i --env_name Walker2d --dataset medium --discrete_control 0 --state_dim 17 --action_dim 6 --tau 0.33 --learning_rate 0.0003 --hidden_units 256 --batch_size 256 --timeout 1000 --max_steps 1000000 --log_interval 10000 &
   sleep 1
   python run_ac_offline.py --seed $i --env_name Walker2d --dataset medrep --discrete_control 0 --state_dim 17 --action_dim 6 --tau 0.5 --learning_rate 0.0003 --hidden_units 256 --batch_size 256 --timeout 1000 --max_steps 1000000 --log_interval 10000 &
-  sleep 1
   wait
+  python plot.py
 done
